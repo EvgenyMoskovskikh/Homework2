0bc1496c27609a58328d9adb8816a1b7a1686749
evgeny moskovskikh <mosevgwork@yandex.ru>
2022-09-20 11:08:35 +0300

    Домашку надо сдавать вовремя

---
+++ Task10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int numberOne = (a % 10);
+int numberThree = (a / 100);
+int numberTwo = (a / 10 % 10);
+Console.Write($"Первая цифра в числе {a} будет {numberThree}, а третья цифра в числе {a} будет {numberOne} и наконец вторая цифра в числе {a} будет {numberTwo}");
